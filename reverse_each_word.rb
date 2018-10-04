0980f2ec1aed5b9c798c4bde02b96bb8ba8b0d2e
scottyd_14 <scottyd_14@hotmail.com>
2018-10-04 20:54:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-# def reverse_each_word(string)
-#   array = string.split(" ")
-#   new_array = []
-#   array.each do |word|
-#     new_array << word.reverse
-#   end
-#   new_string = new_array.join(" ")
-# end
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do |word|
+    new_array << word.reverse
+  end
+  new_string = new_array.join(" ")
+end
  
  def reverse_each_word (string)
    array = string.split(" ").collect {|word|word.reverse} 
