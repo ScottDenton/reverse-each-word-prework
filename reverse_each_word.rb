8dd79a658c296f412d1cf4c63bd82b38f2bd8636
scottyd_14 <scottyd_14@hotmail.com>
2018-10-04 20:51:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,8 +9,7 @@
  
  def reverse_each_word (string)
    array = string.split(" ")
-   array.collect do |word|
-     word.reverse
+   array.collect {|word| word.reverse }
     end
     array.join(" ")
  end
