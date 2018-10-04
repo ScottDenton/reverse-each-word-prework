268a3c62e9110d9edbb15ee8afb58003496f3b45
scottyd_14 <scottyd_14@hotmail.com>
2018-10-04 20:54:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do |word|
+    new_array << word.reverse
+  end
+  new_string = new_array.join(" ")
+end
+ 
+ def reverse_each_word (string)
+   array = string.split(" ").collect {|word|word.reverse} 
+   
+    array.join(" ")
+ end
